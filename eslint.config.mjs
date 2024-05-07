70397e41a674dfc61df54dbbc11048063cf90d3a
Namandeep Singh Wadhwa <naman23156@gmail.com>
2024-05-06 23:17:29 -0400

    eslint added

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+export default [
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+
+      globals: { ...globals.commonjs, ...globals.es2021, ...globals.node },
+    },
+  },
+  pluginJs.configs.recommended,
+];
